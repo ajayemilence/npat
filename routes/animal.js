caf80ca30de0958ab58810cf1f84b32581659641
ajay singh koranga <ajay.emilence@gmail.com>
2018-06-01 17:56:50 +0530

    changes

--- routes/animal.js
+++ routes/animal.js
@@ -29,7 +29,7 @@ router.get('/game', function(req, res) {
 eachdata.createdAt =  moment(eachdata.createdAt).format("l") //"2013-03-10"
             });
         //console.log(data2);
-        res.render('gameAnimal', { words: data , title: 'Animals', rootPath: process.env.IMAGES_ROOT_PATH});
+        res.render('gameAnimal', { words: data , title: 'Animals by game', rootPath: process.env.IMAGES_ROOT_PATH});
     });
 });
 
