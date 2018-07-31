411343466a5e6ad181106b632505792f59e63ead
ajay singh koranga <ajay.emilence@gmail.com>
2018-07-31 16:22:07 +0530

    csv

--- src/middleware/storage.js
+++ src/middleware/storage.js
@@ -2,7 +2,7 @@ let multer = require('multer');
 
 var storage = multer.diskStorage({
 	destination: (req, file, cb) =>{
-		cb(null, './src/uploads/')
+		cb(null, '/root/npatAPI/src/uploads/')
 	},
 	filename : (req, file,cb) => {
 		cb(null, Date.now() + '-' + file.originalname)
